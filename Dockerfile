e211da67843f65609a2d86da44268189db1a2ad7
Hiroyuki Shimabukuro <hiro.carlos93@gmail.com>
2024-03-11 19:37:23 -0300

    fix!: change when create database from register endpoint to when run script db:start BREAKING CHANGE: change docker `compose.yaml` database volume, create database on script `db:start and remove creating database on endpoint register`

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,9 @@
-FROM postgres
-ENV POSTGRES_USER=postgres
-ENV POSTGRES_PASSWORD=local_password
-ENV POSTGRES_DB=local_db
-COPY init.sql /docker-entrypoint-initdb.d/
-
 FROM node
 
 WORKDIR /usr/app
 
 COPY package.json ./
-
+RUN npm install docker
 RUN npm install
 
 COPY . .
--- init.sql
+++ init.sql
@@ -1,5 +1,9 @@
 CREATE USER local_user;
 
-CREATE DATABASE local_database;
+SELECT 'CREATE DATABASE local_db' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'local_db');
 
-GRANT ALL PRIVILEGES ON DATABASE local_database TO local_user;
+GRANT ALL PRIVILEGES ON DATABASE local_db TO local_user;
+
+CREATE TABLE IF NOT EXISTS public.users (id serial PRIMARY KEY, name varchar(255) NOT NULL, email varchar(255) NOT NULL, password varchar(255), created_at timestamp NOT NULL DEFAULT NOW());
+
+ALTER TABLE public.users OWNER TO local_user;
